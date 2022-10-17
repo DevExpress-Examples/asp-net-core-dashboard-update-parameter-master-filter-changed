982dfafd9d2a08e44b922423dfaf8bb830ed7cb3
Polina Tyureva <polina.tyureva@devexpress.com>
2022-10-17 18:22:34 +0400

    add cs solution

---
+++ CS/AspNetCoreDashboard/wwwroot/js/Script.js
@@ -0,0 +1,15 @@
+﻿var dashboardControl;
+function onBeforeRender(sender) {
+    dashboardControl = sender;
+    var viewerApiExtension = dashboardControl.findExtension('viewerApi');
+    if (viewerApiExtension)
+        viewerApiExtension.on('itemMasterFilterStateChanged', onItemMasterFilterStateChanged);
+}
+function onItemMasterFilterStateChanged(e) {
+    if (e.itemName == "gridDashboardItem1") {
+        var dashboardParameterDialogExtension = dashboardControl.findExtension('dashboardParameterDialog');
+        var parameters = dashboardParameterDialogExtension.getParameters();
+        var parameter1 = parameters.getParameterByName("OrderID");
+        parameter1.setValue(e.values[0][0]);
+    }
+}
